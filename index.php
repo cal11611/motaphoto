822a051ad6faeb00fe92c6b0ad2aa3fbd325668a
pascal <cal11611@gmail.com>
2026-04-08 09:57:11 +0200

    init

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
- Hello everybody   
-</body>
-</html>
+<?php get_header() ?>
+ Hello everybody ... coucou  
+<?php get_footer() ?>
 
 
